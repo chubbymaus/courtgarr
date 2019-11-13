52a974532ca1af9a8fd8fd9df6ed942f23d5ca08
chubbymaus <court@chubbymaus.com>
2019-11-13 09:53:09 -0700

    uncommented missing header img

--- src/components/Home/Jumbotron.js
+++ src/components/Home/Jumbotron.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import styled from 'styled-components';
-import JumboImage from '../../images/coding2.png';
+import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
+import JumboImage from "../../images/coding2.png";
 const Button = styled.a.attrs({
-  className: 'button'
+  className: "button"
 })`
   width: 180px;
   margin-top: 0.5rem;
@@ -35,38 +35,38 @@ const HeroText = styled.div`
 
 const Jumbotron = () => (
   <section
-    className='hero has-background-info is-fullheight is-bold'
-    id='jumbo-bg'
+    className="hero has-background-info is-fullheight is-bold"
+    id="jumbo-bg"
   >
-    <div className='hero-body'>
-      <div className='container has-text-left'>
-        <div className='columns is-centered'>
-          <HeroText className='column'>
-            <h1 className='title has-text-primary has-text-weight-bold '>
-              <span className='half-highlight is-size-1'>
+    <div className="hero-body">
+      <div className="container has-text-left">
+        <div className="columns is-centered">
+          <HeroText className="column">
+            <h1 className="title has-text-primary has-text-weight-bold ">
+              <span className="half-highlight is-size-1">
                 Hi! My Name Is Court
               </span>
             </h1>
-            <h2 className='has-text-weight-light subtitle has-text-primary is-size-2'>
+            <h2 className="has-text-weight-light subtitle has-text-primary is-size-2">
               I'm a Fullstack Developer and Designer...
             </h2>
             <ButtonGroup>
               <Button
-                className='is-primary is-outlined is-medium'
-                href='#skills'
+                className="is-primary is-outlined is-medium"
+                href="#skills"
               >
                 Learn More
               </Button>
-              <Link to='/blog'>
-                <Button className='is-outlined is-primary is-medium'>
+              <Link to="/blog">
+                <Button className="is-outlined is-primary is-medium">
                   Visit My Blog
                 </Button>
               </Link>
             </ButtonGroup>
           </HeroText>
-          {/* <div className='column has-text-centered' id='header-image'>
+          <div className="column has-text-centered" id="header-image">
             <img src={JumboImage} />
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
